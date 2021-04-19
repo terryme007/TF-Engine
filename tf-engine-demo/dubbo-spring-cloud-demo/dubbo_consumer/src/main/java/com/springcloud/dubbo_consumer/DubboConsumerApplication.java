c63f17026dfcd60fbbfd32df069c4b18b305c30c
terrycr <rui.c@sunyard.com>
2021-04-20 01:31:38 +0800

    完成dubbo生产者消费者自动注册，实现dubbo消费者调用，新增获取注册关系接口。其他的有时间再优化。

--- tf-engine-demo/dubbo-spring-cloud-demo/dubbo_consumer/src/main/java/com/springcloud/dubbo_consumer/DubboConsumerApplication.java
+++ tf-engine-demo/dubbo-spring-cloud-demo/dubbo_consumer/src/main/java/com/springcloud/dubbo_consumer/DubboConsumerApplication.java
@@ -1,11 +1,15 @@
 package com.springcloud.dubbo_consumer;
 
+import goway.me.tfengine.core.annotation.TEngineClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@TEngineClient(enable = true)
+@ComponentScan({"goway.me.tfengine.core","com.springcloud"})
 public class DubboConsumerApplication {
 
     public static void main(String[] args) {
--- tf-engine-demo/dubbo-spring-cloud-demo/dubbo_consumer/src/main/java/com/springcloud/dubbo_consumer/controller/HelloController.java
+++ tf-engine-demo/dubbo-spring-cloud-demo/dubbo_consumer/src/main/java/com/springcloud/dubbo_consumer/controller/HelloController.java
@@ -1,21 +1,25 @@
 package com.springcloud.dubbo_consumer.controller;
 
 import com.springcloud.dubbo_api.service.TEngineService;
-import com.springcloud.dubbo_consumer.utils.Base64FileUtil;
-import com.springcloud.dubbo_consumer.utils.DubboRemoteUtil;
-import com.springcloud.dubbo_consumer.utils.SpringContextHolder;
-import org.apache.dubbo.config.RegistryConfig;
+import goway.me.tfengine.core.model.NodeData;
+import goway.me.tfengine.core.model.RegistryData;
+import goway.me.tfengine.core.utils.APILib;
+import goway.me.tfengine.core.utils.Base64FileUtil;
+import goway.me.tfengine.core.utils.DubboRemoteUtil;
 import org.apache.dubbo.config.annotation.Reference;
 import org.apache.dubbo.config.spring.ReferenceBean;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -27,14 +31,6 @@ import java.util.*;
  */
 @RestController
 public class HelloController {
-
-//    @Reference(check = false,loadbalance = "demo",url = "dubbo://localhost:9999")
-//    private HelloService helloService;
-//
-//    @GetMapping("/hello")
-//    public String hello() {
-//        return helloService.hello("Dubbo!");
-//    }
 
     @Reference(check = false,version = "1.0")
     private TEngineService engineService;
@@ -63,55 +59,20 @@ public class HelloController {
         return DubboRemoteUtil.remoteInvoke(invokeMethod,methodParams,version);
     }
 
-    @GetMapping("/downAndRegister")
-    public boolean downAndRegister(String apiReference, String modelName, String version){
-        String fileBase64 = engineService.downApiJar(apiReference, modelName, version);
-        if(fileBase64!=null){
-            try {
-                String savePath="apiJar/"+apiReference.substring(apiReference.lastIndexOf(".")+1)+"_"+version+".jar";
-                boolean saveSuccess=Base64FileUtil.generateFile(fileBase64,savePath);
-                if(saveSuccess){
-                    String packagePath="com.springcloud.dubbo_api.service";
-                    DubboRemoteUtil.initService(savePath,version,packagePath);
-                }else{
-                    return false;
-                }
-                return true;
-            } catch (Exception e) {
-                e.printStackTrace();
-                return false;
-            }
-        }else{
-            return false;
-        }
-    }
-
-    @GetMapping("/init")
-    public String initRemoteService(String jarPath){
-        jarPath=jarPath.replaceAll("\\.","/");
-        String version="1.0";
-        String packagePath="com.springcloud.dubbo_api.service";
-        System.out.println("jarPath:"+jarPath);
-        int successCount = DubboRemoteUtil.initService(jarPath,version,packagePath);
-        if(successCount==0){
-            System.out.println("类初始化加载失败！");
-        }
-        return "成功加载"+successCount+"个类";
-    }
-
     @GetMapping("/getInvokeMap")
     public Map<String,Map<String,String>> getInvokeMap(){
         return DubboRemoteUtil.getInvokeMap();
     }
 
-    @GetMapping("/showClassPath")
-    public void main(String[] args) {
-        System.out.println(System.getProperty("java.class.path"));
-        URL[] urLs = ((URLClassLoader) ClassLoader.getSystemClassLoader()).getURLs();
-        for(URL url:urLs){
-            System.out.println(url.getPath());
-        }
+    @GetMapping("/getRegistryList")
+    public List<NodeData> getRegistryList(){
+        return APILib.getNodeDataMap().stream().map(nodeData -> {
+            NodeData newNodeData=new NodeData();
+            BeanUtils.copyProperties(nodeData,newNodeData);
+            newNodeData.getRegistryData().setJarData(null);
+            newNodeData.getRegistryData().setUsername(null);
+            newNodeData.getRegistryData().setPassword(null);
+            return newNodeData;
+        }).collect(Collectors.toList());
     }
-
-
 }
